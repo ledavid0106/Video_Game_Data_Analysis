85514e3da069bb2ff26dd1d680b118d550a000d0
David Le <104813594+ledavid0106@users.noreply.github.com>
2022-11-30 13:31:52 -0800

    Resolved JapanVsGlobal Sales Chart DS

--- video_game_data_analysis/src/Components/Evaluation/Evaluation.jsx
+++ video_game_data_analysis/src/Components/Evaluation/Evaluation.jsx
@@ -4,56 +4,35 @@ import { Chart } from "react-google-charts";
 import randomColor from "randomcolor";
 
 const Evaluation = ({gamedata}) => {
-    function getGlobalSales(){
-        let filteredGames = gamedata.filter(game=>game.year >= 2016);
-        let platforms = filteredGames.map(game=> game.platform)
-        let distinctPlatform = [...new Set(platforms)]
-        let platformArrays = distinctPlatform.map(plat => {
-            let allGamesForPlatform = filteredGames.filter(game=> game.platform == plat)
-            let globalSale = 0
-            for (let i = 0; i < allGamesForPlatform.length;i++){
-                globalSale += allGamesForPlatform[i]["globalsales"]
-            }
-           return [plat , globalSale, "red"]
-        })
-        const data = [
-            ['Platform', 'Sales in Millions', {role: "style"}],
-            ...platformArrays,
-          ];
-        return data
-    }
-    function getJapanSales(){
+    function getGJSales(){
         let filteredGames = gamedata.filter(game=>game.year >= 2016);
         let platforms = filteredGames.map(game=> game.platform)
         let distinctPlatform = [...new Set(platforms)]
         let platformArrays = distinctPlatform.map(plat => {
             let allGamesForPlatform = filteredGames.filter(game=> game.platform == plat)
             let japanSale = 0
+            let globalSale = 0
             for (let i = 0; i < allGamesForPlatform.length;i++){
                 japanSale += allGamesForPlatform[i]["japansales"]
+                globalSale += allGamesForPlatform[i]["globalsales"]
             }
-           return [plat , japanSale, "red"]
+           return [plat , japanSale, globalSale,]
         })
         const data = [
-            ['Platform', 'Sales in Millions', {role: "style"}],
+            ['Platform', "Japan Sales", "Global Sales",],
             ...platformArrays,
           ];
         return data
     }
-    const japan = getJapanSales();
-    const global = getGlobalSales();
-    const diffdata = {
-        old: global,
-        new: japan,
-      };
+
         
     return ( 
 
         <div>
-        <h2>Japan Sales By Console After 2016</h2> {/* //please center later// */}
+        <h2>Japan Sales Vs Global Sales By Console After 2016</h2> {/* //please center later// */}
         <Chart
             chartType="ColumnChart"
-            diffdata={diffdata}
+            data={getGJSales()}
             width="100%"
             height="400px"
             legendToggle
